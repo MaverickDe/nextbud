ed95b03f208d9d5c1c7058163f402645fb893489
princewill Asotibe <princewillasotibe123@gmail.com>
2024-05-30 00:57:30 +0100

    new feat

--- components/Home/Solution.js
+++ components/Home/Solution.js
@@ -27,7 +27,7 @@ const Overview = () => {
       <h1 className="my-5 max-lg:text-[50px] max-md:text-[40px] text-[70px]">
       Join us as we embark on a journey of connection, empowerment, and celebration.
         </h1>
-        <button className="text-[#9864F1] rounded-md px-5 py-3 my-5 text-bold border-[1px] border-[#9864F1] w-fit">Get the app</button>
+        <button className="text-[#9864F1] rounded-md px-5 py-3 my-5 text-bold border-[1px] border-[#9864F1] w-fit">Get started as an Influencer</button>
         
       </div>
       <div className="w-[50%] max-md:w-full overflow-clip">
